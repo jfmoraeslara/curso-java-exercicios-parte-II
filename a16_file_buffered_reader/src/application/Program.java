e70762775a896aa31cea2610777111ccd202cb59
João Fernando <70722165+jfmoraeslara@users.noreply.github.com>
2021-12-08 14:23:46 -0300

    a16_bufferedReader_reduzido

--- a16_file_buffered_reader/src/application/Program.java
+++ a16_file_buffered_reader/src/application/Program.java
@@ -7,17 +7,12 @@ import java.io.IOException;
 public class Program {
 
 	public static void main(String[] args) {
-		
-		// implantação de forma manual
+				
 		String path = "C:\\Users\\User\\Desktop\\Uniasselvi\\JAVA Udemy\\in.txt";
-		FileReader fr = null;
-		BufferedReader br = null;
 		
-		try {
-			fr = new FileReader(path);
-			br = new BufferedReader(fr);
-			
-			String line	= br.readLine();
+		// implantação direta com BufferedReader		
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+			String line = br.readLine();
 			
 			while (line != null) {
 				System.out.println(line);
@@ -26,19 +21,6 @@ public class Program {
 		}
 		catch (IOException e) {
 			System.out.println("Error: " + e.getMessage());
-		}
-		finally {
-			try {
-				if (br != null) {
-					br.close();
-				}
-				if (fr != null) {
-					fr.close();
-				}
-			}
-			catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
+		}		
 	}
 }
